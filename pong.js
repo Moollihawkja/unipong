3006fce599e4e5197fbbe96df2bee2ffa2091e8b
Moollihawkja <jared@wisen.space>
2019-05-24 18:26:13 -0700

    added ball variables

--- pong.js
+++ pong.js
@@ -20,7 +20,9 @@ let rightPosition = 44;
 let paddleHeight = 12;
 let leftSpeed = 0;
 let rightSpeed = 0;
-
+let ballSpeed = 1;
+let angle;
+let direction;
 let players = [];
 
 /* MIDDLEWARE TO LOOK AT THE REQUEST BEFORE HANDLING IT */
@@ -47,13 +49,19 @@ function startSocketServer() {
         }
 
         if (players.length === 2) {
+            const π = Math.PI;
+            direction = Math.random() <= 0.5 ? -1 : 1; //RANDOMLY CHOOSE A NUMBER THAT IS -1 or 1
+            angle = (Math.random() - 0.5) * 2 * π / 3;  //RANDOMLY CHOOSE A NUMBER THAT IS BETWEEN -pi/4 and pi/4
             io.emit('start', {
                 speed,
                 leftPosition,
                 rightPosition,
                 paddleHeight,
                 leftSpeed,
-                rightSpeed
+                rightSpeed,
+                angle,
+                direction,
+                ballSpeed
             });
         }
 
